1c92d79476a13a36726a99b0d0f4eb626af4fcd6
James Robertson <james@jamesrobertson.me.uk>
2021-02-23 12:09:03 +0000

    feature: Added the public method *elapsed( which returns the elapsed time in a humanized format.

--- lib/unichron.rb
+++ lib/unichron.rb
@@ -3,23 +3,50 @@
 # file: unichron.rb
 
 require 'chronic_cron'
+require 'christian_calendar'
 
 
 class Unichron
 
   attr_reader :r
 
-  def initialize(obj)
+  def initialize(obj, debug: false)
 
+    @debug = debug
     @r = select_method(obj)
+    
+  end
+
+  # Returns the elapsed time in a humanized format
+  # e.g. elapsed(Time.now - 4000) #=> 1h
+  # note: An elapsed time more than a day is returned as a calendar date
+  #
+  def elapsed()
+    
+    elapsed = Time.now - @r
+
+    relative_time = if elapsed < Time.now - (Date.today - 1).to_time then
+      Subunit.seconds(elapsed).strfunit("%s")
+    else
+      t2.strftime("%b %d")
+    end    
+    
   end
 
   def to_seconds
     @r
   end
+  
+  def to_date
+    @r.to_date if valid?
+  end  
 
   def to_time
-    @r.to_time
+    @r.to_time if valid?
+  end
+  
+  def valid?
+    @r.respond_to? :to_time
   end
 
   private
@@ -30,16 +57,26 @@ class Unichron
 
       s = obj
       r = Chronic.parse(s)
+      puts 'r1: ' + r.inspect if @debug      
       return r if r
       
       r = ChronicCron.new s
+      puts 'r2: ' + r.inspect if @debug
       return r if r.valid?
 
       r = ChronicDuration.parse s
+      puts 'r3: ' + r.inspect if @debug
+      return r if r
+      
+      r = ChristianCalendar.new.query(obj)
+      puts 'r4 ' + r.inspect if @debug      
       return r if r
 
+      
       return nil
 
+    else
+      obj
     end
   end
 
--- unichron.gemspec
+++ unichron.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name = 'unichron'
-  s.version = '0.2.0'
+  s.version = '0.2.1'
   s.summary = 'A universal chron tool.'
   s.authors = ['James Robertson']
   s.files = Dir['lib/unichron.rb']
