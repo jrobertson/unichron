eff9ee8c58f6973b591fe59386563f72f82e6f8b
James Robertson <digital.robertson@gmail.com>
2022-03-04 16:49:21 +0000

    bug fix: Changed condition to use ==.

--- lib/unichron.rb
+++ lib/unichron.rb
@@ -15,7 +15,7 @@ class Unichron
 
     @mode, @debug = mode, debug
     @r = select_method(obj)
-    
+
   end
 
   # Returns the elapsed time in a humanized format
@@ -23,29 +23,29 @@ class Unichron
   # note: An elapsed time more than a day is returned as a calendar date
   #
   def elapsed(limit=:day)
-    
+
     elapsed = Time.now - @r
 
     relative_time = if elapsed < Time.now - (Date.today - 1).to_time then
       Subunit.seconds(elapsed).strfunit("%s")
     else
       @r.strftime("%b %d")
-    end    
-    
+    end
+
   end
 
   def to_seconds
     @r
   end
-  
+
   def to_date
     @r.to_date if valid?
-  end  
-  
+  end
+
   def to_datetime
     @r.to_datetime if valid?
-  end    
-  
+  end
+
   def to_i()
     @r.to_time.to_i if valid?
   end
@@ -53,7 +53,7 @@ class Unichron
   def to_time
     @r.to_time if valid?
   end
-  
+
   def valid?
     @r.respond_to? :to_time
   end
@@ -65,16 +65,16 @@ class Unichron
     if obj.is_a? String then
 
       s = obj
-      
-      r = if @mode = :little_endian
+
+      r = if @mode == :little_endian
         Chronic.parse(s, :endian_precedence => :little)
       else
         Chronic.parse(s)
       end
-      
-      puts 'r1: ' + r.inspect if @debug      
+
+      puts 'r1: ' + r.inspect if @debug
       return r if r
-      
+
       r = ChronicCron.new s
       puts 'r2: ' + r.inspect if @debug
       return r if r.valid?
@@ -82,14 +82,14 @@ class Unichron
       r = ChronicDuration.parse s
       puts 'r3: ' + r.inspect if @debug
       return r if r
-      
+
       r = ChristianCalendar.new.query(obj)
-      puts 'r4 ' + r.inspect if @debug      
+      puts 'r4 ' + r.inspect if @debug
       return r if r
 
-      
+
       return nil
-      
+
     elsif obj.is_a? Integer
       r = Time.at obj
       return r
@@ -101,14 +101,14 @@ class Unichron
 end
 
 module UnichronUtils
-  
+
   class Quicktime
 
     attr_reader :time
 
     def initialize(time: Time.now, speed: 4)
 
-      @time, @speed = Unichron.new(time).to_time, speed  
+      @time, @speed = Unichron.new(time).to_time, speed
 
     end
 
@@ -116,9 +116,9 @@ module UnichronUtils
 
       @state = :play
 
-      Thread.new do 
+      Thread.new do
 
-        loop do 
+        loop do
           (sleep 0.5; next) if @state == :pause
           @time += 1
           sleep 1.0 / @speed
--- unichron.gemspec
+++ unichron.gemspec
@@ -1,11 +1,11 @@
 Gem::Specification.new do |s|
   s.name = 'unichron'
-  s.version = '0.4.0'
+  s.version = '0.4.1'
   s.summary = 'A universal chron tool.'
   s.authors = ['James Robertson']
   s.files = Dir['lib/unichron.rb']
   s.add_runtime_dependency('subunit', '~> 0.8', '>=0.8.5')
-  s.add_runtime_dependency('chronic_cron', '~> 0.6', '>=0.6.2')
+  s.add_runtime_dependency('chronic_cron', '~> 0.7', '>=0.7.1')
   s.add_runtime_dependency('christian_calendar', '~> 0.1', '>=0.1.7')
   s.signing_key = '../privatekeys/unichron.pem'
   s.cert_chain  = ['gem-public_cert.pem']
